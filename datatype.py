adf7ac3cf6960a560c3ff17429ad315b07202e0f
Vinay Yadav <vy20409@gmail.com>
2022-07-31 14:05:04 +0530

    basics

---
+++ datatype.py
@@ -0,0 +1,34 @@
+x = 1920
+y = 9203
+x2 = -39021309
+y2 = -9203
+print(x , type(x))
+print(y , type(y))
+print(x2 , type(x2))
+print(y2 , type(y2))
+z = 1.10
+m = -35.59
+n = 87.7e100
+print(z ,type(z))
+print(m , type(m))
+print(n , type(n))
+name = "vinay yadav"
+color = 'black'
+print(name, type(name))
+print(color, type(color))
+u = None
+print(u, type(u))
+a = [5,10,15,20,30,35,40]
+vals = [1,2.2,'python']
+color = ['red','green','yellow']
+print(a, type(a))
+print(vals, type(vals))
+print(color, type(color))
+a = {1,2,3,4}
+print(a, type(a))
+car_info = {
+    "brand": "ford",
+    "model": "mustang",
+    "year": 1964
+}
+print(car_info, type(car_info))
---
+++ operators.py
@@ -0,0 +1,14 @@
+a = 10
+b = 3
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a**b)
+
+color = ['red','Black','green','Yellow']
+print('white'in color)
+print('Black'in color)
+print('black' in color)
